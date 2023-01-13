f39d2c50a5ab66640dd56c156682166a6bf9ce1b
Muhammet Kır <muhammetkir97@gmail.com>
2023-01-14 00:46:00 +0300

    Initial commit

---
+++ Assets/Scripts/Controllers/DrawZoneController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+public class DrawZoneController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+{
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
